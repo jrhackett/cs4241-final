ba1e595661c9172976185316df043928377a7be0
Jacob Hackett <jacobrhackett@gmail.com>
2016-11-21 21:20:40 -0500

    readability

--- app/uploaders/sound_attachment_uploader.rb
+++ app/uploaders/sound_attachment_uploader.rb
@@ -13,12 +13,12 @@ class SoundAttachmentUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-   end
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
    
-   def extension_white_list
-      %w(mp3 ogg)
-   end
+  def extension_white_list
+    %w(mp3 ogg)
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
