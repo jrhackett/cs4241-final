902c1b1c82370457aa55c950507f1cb19745e22c
Jacob Hackett <jacobrhackett@gmail.com>
2016-12-07 16:41:39 -0500

    length validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   before_save { self.name = name.downcase.delete(" ") }
-  validates :name, presence: true, uniqueness: {scope: :board_id}
+  validates :name, presence: true, uniqueness: {scope: :board_id}, length: { minimum: 2, maximum: 16 }
   belongs_to :board
 end
