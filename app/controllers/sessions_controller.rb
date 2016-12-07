6ed371390d0508abc024d78ba6d5c4cef6cf98cf
Jacob Hackett <jacobrhackett@gmail.com>
2016-12-07 15:48:03 -0500

    cleaning up the board and sound show views after merging in some changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_back_or user
-	else
+	  else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
